3932ae21eeb90bb3e27826b8ae16ff832660bbe7
Elias Technik <83211893+EliasTechnik@users.noreply.github.com>
2026-03-24 13:41:42 +0100

    more tests

--- app/src/main/java/dev/ewio/encryptedroom/data/AppDatabase.kt
+++ app/src/main/java/dev/ewio/encryptedroom/data/AppDatabase.kt
@@ -35,14 +35,27 @@ abstract class AppDatabase : RoomDatabase() {
         private fun buildDatabase(context: Context): AppDatabase {
             Log.d("AppDatabase", "building encrypted database...")
 
+            /*
+            Note: PRAGMA cipher_default_page_size = 8192; will in postKey will be executed only on future connections. This means that this pragma isnt applied when the database is created.
+            Removing the pragma removes the bug. Applying the PRAGMA cipher_page_size = 4096; in preKey should in theory work because 4096 is the default (and I assume it is used when creating the database).
+            In reality it doesnt. Why I dont know.
+
+            For the PRAGMA cipher_use_hmac = OFF; the story is different. From my tests it must stay in postKey. TODO: Test PRAGMA cipher_default_use_hmac = OFF; in preKey instead.
+             */
+
+
             val hook = object : SQLiteDatabaseHook {
-                override fun preKey(connection: SQLiteConnection) {}
+                override fun preKey(connection: SQLiteConnection) {
+                    //connection.execute("PRAGMA cipher_page_size = 4096;", null, null)
+
+                }
 
                 override fun postKey(connection: SQLiteConnection) {
                     //Note: Changing this breaks existing databases. This is not fixable by common migrations!
-                    connection.execute("PRAGMA cipher_default_page_size = 8192;", null, null)
+                    //connection.execute("PRAGMA cipher_default_page_size = 8192;", null, null)
+                    //This needs to stay here in order for the DB to not break.
                     connection.execute("PRAGMA cipher_use_hmac = OFF;",  null, null)// see: https://www.zetetic.net/sqlcipher/performance/#:~:text=entropy%20key%20values.-,Disable,-Page%20Data%20Validation
-                    connection.execute("PRAGMA cipher_memory_security = OFF;",  null, null) //turns improved sanitization of in-memory data used by the database off.
+                    //connection.execute("PRAGMA cipher_memory_security = OFF;",  null, null) //turns improved sanitization of in-memory data used by the database off.
                     //This increases the risk of memory readouts but again improves performance. This risk is generally accepted: https://www.zetetic.net/sqlcipher/performance/#:~:text=Turn%20Off%20Memory%20Security
                 }
             }
@@ -51,13 +64,13 @@ abstract class AppDatabase : RoomDatabase() {
 
             val factory = SupportOpenHelperFactory(passphrase, hook, true)
 
-            val queryExecutor = Executors.newFixedThreadPool(2) //change this to 1 and the database works everytime
+            val queryExecutor = Executors.newFixedThreadPool(1) //change this to 1 and the database works everytime
             val transactionExecutor = Executors.newSingleThreadExecutor()
 
             return Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
                 .openHelperFactory(factory)
-                .setQueryExecutor(queryExecutor)
-                .setTransactionExecutor(transactionExecutor)
+                //.setQueryExecutor(queryExecutor)
+                //.setTransactionExecutor(transactionExecutor)
                 //.setJournalMode(JournalMode.TRUNCATE) //this also fixes the issue but disables WAL
                 .addCallback(object : RoomDatabase.Callback() {
                     override fun onCreate(db: SupportSQLiteDatabase) {
